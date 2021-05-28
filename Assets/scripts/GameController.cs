2f9da4510ede134d75b35a9cd532485548918729
maxim <maximum454@gmail.com>
2021-05-28 17:55:33 +0600

    first commit

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -3,10 +3,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-public class GameControler : MonoBehaviour{
+public class GameController : MonoBehaviour{
     private CubePos nowCube = new CubePos(0, 1, 0);
     public float cubeChangePlaceSpeed = 0.5f;
     public Transform cubeToPlace;
+
+    public GameObject cubeToCreate, allCubes;
+
+    private Rigidbody allCubesRb;
+
+    private bool IsLose;
 
     private List<Vector3> allCubesPositions = new List<Vector3> {
         new Vector3(0,0,0),
@@ -21,8 +27,40 @@ public class GameControler : MonoBehaviour{
         new Vector3(1,0,-1),
     };
 
+    private Coroutine showCubePlace;
+
     private void Start() {
-        StartCoroutine(ShowCubePlace());
+        allCubesRb = allCubes.GetComponent<Rigidbody>();
+        showCubePlace = StartCoroutine(ShowCubePlace());
+    }
+
+    private void Update() {
+        if((Input.GetMouseButtonDown(0) || Input.touchCount >0) && cubeToPlace != null){
+            #if !UNITY_EDITOR
+                if(Input.GetTouch(0).phase != TouchPhase.Began)
+                return;
+            #endif
+
+            GameObject newCube = Instantiate(
+                cubeToCreate,
+                cubeToPlace.position,
+                Quaternion.identity) as GameObject;
+
+                newCube.transform.SetParent(allCubes.transform);
+                nowCube.setVector(cubeToPlace.position);
+                allCubesPositions.Add(nowCube.getVector());
+
+                allCubesRb.isKinematic = true;
+                allCubesRb.isKinematic = false;
+
+                SpawnPositions();
+        }
+
+        if(!IsLose && allCubesRb.velocity.magnitude > 0.1f){
+            Destroy(cubeToPlace.gameObject);
+            IsLose = true;
+            StopCoroutine(showCubePlace);
+        }
     }
 
     IEnumerator ShowCubePlace(){
@@ -35,19 +73,25 @@ public class GameControler : MonoBehaviour{
 
     private void SpawnPositions(){
         List<Vector3> positions = new List<Vector3>();
-        if( IsPositionEmpty (new Vector3(nowCube.x + 1, nowCube.y, nowCube.z) ))
+        if( IsPositionEmpty (new Vector3(nowCube.x + 1, nowCube.y, nowCube.z) )
+            && nowCube.x + 1 != cubeToPlace.position.x)
             positions.Add(new Vector3(nowCube.x + 1, nowCube.y, nowCube.z));
-        if( IsPositionEmpty (new Vector3(nowCube.x + 1, nowCube.y, nowCube.z) ))
+        if( IsPositionEmpty (new Vector3(nowCube.x - 1, nowCube.y, nowCube.z) )
+            && nowCube.x - 1 != cubeToPlace.position.x)
             positions.Add(new Vector3(nowCube.x - 1, nowCube.y, nowCube.z));
 
-        if( IsPositionEmpty (new Vector3(nowCube.x + 1, nowCube.y, nowCube.z) ))
+        if( IsPositionEmpty (new Vector3(nowCube.x, nowCube.y + 1, nowCube.z) )
+        && nowCube.y + 1 != cubeToPlace.position.y)
             positions.Add(new Vector3(nowCube.x, nowCube.y+1, nowCube.z));
-        if( IsPositionEmpty (new Vector3(nowCube.x + 1, nowCube.y, nowCube.z) ))
+        if( IsPositionEmpty (new Vector3(nowCube.x, nowCube.y-1, nowCube.z) )
+        && nowCube.y - 1 != cubeToPlace.position.y)
             positions.Add(new Vector3(nowCube.x, nowCube.y-1, nowCube.z));
 
-        if( IsPositionEmpty (new Vector3(nowCube.x + 1, nowCube.y, nowCube.z) ))
+        if( IsPositionEmpty (new Vector3(nowCube.x, nowCube.y, nowCube.z+1) )
+        && nowCube.z + 1 != cubeToPlace.position.z)
             positions.Add(new Vector3(nowCube.x, nowCube.y, nowCube.z + 1));
-        if( IsPositionEmpty (new Vector3(nowCube.x + 1, nowCube.y, nowCube.z) ))
+        if( IsPositionEmpty (new Vector3(nowCube.x, nowCube.y, nowCube.z-1) )
+        && nowCube.z - 1 != cubeToPlace.position.z)
             positions.Add(new Vector3(nowCube.x, nowCube.y, nowCube.z-1));
         
         
